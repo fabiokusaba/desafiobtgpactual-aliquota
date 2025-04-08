7f6a75ccdead08edd73638a9a97967df7ac0c188
Fabio Kusaba <fabinhokb44@gmail.com>
2025-04-07 23:04:09 -0300

    feat: adicionar contexto da aplicação, relacionamento entre entidades e seed de dados

---
+++ Aliquota/Data/AppDbContext.cs
@@ -0,0 +1,47 @@
+using Aliquota.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aliquota.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<FundoInvestimento> Fundos { get; set; }
+    public DbSet<Aplicacao> Aplicacoes { get; set; }
+    public DbSet<Resgate> Resgates { get; set; }
+    public DbSet<Cliente> Clientes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Aplicacao>()
+            .HasOne<FundoInvestimento>(a => a.FundoInvestimento)
+            .WithMany(fi => fi.Aplicacoes)
+            .HasForeignKey(ap => ap.FundoId);
+        
+        modelBuilder.Entity<Aplicacao>()
+            .HasOne<Cliente>(a => a.Cliente)
+            .WithMany(cl => cl.Aplicacoes)
+            .HasForeignKey(ap => ap.ClienteId);
+        
+        modelBuilder.Entity<Resgate>()
+            .HasOne<FundoInvestimento>(r => r.FundoInvestimento)
+            .WithMany(fi => fi.Resgates)
+            .HasForeignKey(rs => rs.FundoId);
+        
+        modelBuilder.Entity<Resgate>()
+            .HasOne<Cliente>(r => r.Cliente)
+            .WithMany(c => c.Resgates)
+            .HasForeignKey(rs => rs.ClienteId);
+
+        modelBuilder.Entity<FundoInvestimento>().HasData(
+            new FundoInvestimento("Fundo de Investimento A") { Id = 1 },
+            new FundoInvestimento("Fundo de Investimento B") { Id = 2 },
+            new FundoInvestimento("Fundo de Investimento C") { Id = 3 }
+        );
+
+        modelBuilder.Entity<Cliente>().HasData(
+            new Cliente("Ronaldo") { Id = 1 },
+            new Cliente("Jose") { Id = 2 },
+            new Cliente("Maria") { Id = 3 }
+        );
+    }
+}
